a2834f24c2c17bdafa0c32544f74cf0a6fa3c7f7
Bartosz Ślusarczyk <125917295+BartoszSl@users.noreply.github.com>
2023-10-05 08:36:40 +0200

    Delete CloteShoe directory

--- CloteShoe/src/js/other.js
+++
@@ -1,9 +0,0 @@
-const currentYear = document.querySelector('.currentYear');
-
-const currentYearFunction = () => {
-	const data = new Date().getFullYear();
-	currentYear.textContent = data;
-};
-
-window.addEventListener('load', currentYearFunction);
-
